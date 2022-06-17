f4b675e069e1baa8fe33e733ebf548a71f632b9b
Marina <marina.zilkina@gmail.com>
2022-06-17 22:01:45 +0300

    Initial commit

---
+++ NewsPaper/NewsPortal/templatetags/cencor_filter.py
@@ -0,0 +1,14 @@
+from django import template
+
+register = template.Library()
+
+CENSOR_LIST = ['выход', 'спикер', 'список', 'роль', 'продукты']
+
+@register.filter(name='censor')
+def censor(value):
+    for word in value.split():
+        if word.lower() in CENSOR_LIST:
+            x = word.replace(word[1:], '*' * (len(word)-1))
+            value = value.replace(word, x)
+            return value
+
